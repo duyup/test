17aae75001be4497275ee6fd5ebaf1c94efcf528
[duyup] <[chado123@naver.com]>
2021-09-09 19:24:49 +0900

    잿슨나노 -> 아두이노

---
+++ chado.py
@@ -0,0 +1,23 @@
+import serial
+import time
+
+arduino= serial.Serial('/dev/ttyACM0',9600)
+Trans="Q"
+Trans= Trans.encode('utf-8')
+
+while 1:
+    f = open("send.txt",'r')
+    line = f.readline()
+    
+    if line == 'kidnap':
+       arduino.write(Trans)
+       print(line)
+       time.sleep(0.5)
+
+
+
+
+
+
+
+
